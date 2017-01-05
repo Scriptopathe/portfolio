b54fde385aee0e15cbea9b93987a223b211e2bec
Scriptopathe <josue.alvarez@hotmail.fr>
2017-01-05 01:03:54 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
 user=$1
 echo "Build..."
-ng build --base-href /~$user/portfolio/dist/
+if [ "$2" != "nobuild" ]
+then
+	ng build --base-href /~$user/portfolio/dist/
+fi
 echo "Tar..."
 tar -cvf dist.tar dist/
 echo "Scp"
 scp dist.tar $user@etud.insa-toulouse.fr:~/portfolio.tar
 echo "SSH"
 ssh $user@etud.insa-toulouse.fr << EOSSH
+rm -rf public_html/portfolio
+mkdir public_html/portfolio
 tar -xvf portfolio.tar -C public_html/portfolio
 exit
 EOSSH
